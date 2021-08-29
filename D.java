8e6981b9f99525e4fa5813d220f055f4f58dc798
GhabriellLuz <ghabriell.luz@hotmail.com>
2021-08-29 15:09:03 -0300

    Criado a Classe D

---
+++ D.java
@@ -0,0 +1,16 @@
+public class D {
+  private int D1;
+  private boolean D2;
+
+  public int MD1(int d1) {
+    D1 = d1;
+    return D1 * D1;
+  }
+
+  public String MD2(boolean d2) {
+    D2 = d2;
+    return D2 ? "Verdadeiro" : "Falso";
+  }
+}
+
+// Ghabriell Luz
